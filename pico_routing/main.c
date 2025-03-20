fedacba2287ca2c29079ac9c445715fe318f9cfe
Dimitar <d.dimitrov2@student.tue.nl>
2025-03-20 15:58:10 +0100

    First implement of generic logic

--- pico_routing/main.c
+++ pico_routing/main.c
@@ -6,106 +6,244 @@
 
 #include <stdbool.h>
 
-void demo() {
-	print("start\n");
-	led_set_color(0xff0000);
-	belt_big_set_speed(0);
-	belt_small_set_speed(0);
-	servo_angle_set(0);
-	subscribe_to_event(EVENT_RFID_DETECT);
-	enum EventType e;
-	while (1) {
-		e = next_event();
-		if (e == EVENT_RFID_DETECT) {
-			rfid_to_laser_right();
-			sleep(1000);
-			laser_right_to_rfid();
-			sleep(1000);
-			rfid_to_laser_left();
-			sleep(1000);
-			laser_left_to_rfid();
-			sleep(1000);
-			rfid_to_laser_right();
-			sleep(1000);
-			laser_right_to_laser_left();
-			sleep(1000);
-			laser_left_to_laser_right();
-			subscribe_to_event(EVENT_RFID_DETECT);
-			//swap_laser_2_and_rfid();
-			//subscribe_to_event(EVENT_LASER_LEFT_DETECT);
-			print("end\n");
-		}
-		sleep(100);
+#define MAX_MODULES 100
+#define MAX_PLANES 256
+
+int SECOND = 1000000;
+
+// Enum for task
+typedef enum
+{
+	IDLE,
+	MOVE_RFID_TO_LEFT,
+	MOVE_LEFT_TO_RFID,
+
+	MOVE_RFID_TO_RIGHT,
+	MOVE_RIGHT_TO_RFID,
+
+	MOVE_LEFT_TO_RIGHT,
+	MOVE_RIGHT_TO_LEFT,
+
+	LEAVE_AT_LEFT,
+	LEAVE_AT_RIGHT,
+	LEAVE_AT_RFID,
+
+	WAIT_AT_RFID,
+	WAIT_AT_LEFT,
+	WAIT_AT_RIGHT
+} Move;
+
+// Structure for a request
+typedef struct
+{
+    int sender_ID;
+    int type;
+    // Data
+    bool security_status;
+    bool plane_or_dropoff;
+    int  plane_ID;
+    bool payload;
+    bool plane_arrived;
+    int  destination;
+
+} Request;
+Request current_request;
+
+typedef struct
+{
+	Move move;
+	Request request;
+} Task;
+Task tasks[10];
+int current;
+int next_free;
+
+// Neighbouring modules
+typedef struct
+{
+    int left;
+    int right;
+    int rfid;
+} NextModule;
+NextModule next;
+
+typedef enum
+{
+	LASER_LEFT,
+	LASER_RIGHT,
+	RFID
+} ModulePoints;
+
+typedef struct
+{
+	bool at_left;
+	bool at_right;
+	bool at_RFID;
+} State;
+State state;
+
+// Unused for now
+State projected_state;
+
+int planes[MAX_PLANES];
+int lookup[MAX_MODULES];
+
+void init(int my_id) {
+	state.at_left = false;
+	state.at_right = false;
+	state.at_RFID = false;
+
+	for(int i = 0; i < MAX_PLANES; i++) {
+		planes[i] = 0;
+	}
+
+	for(int i = 0; i < MAX_MODULES; i++) {
+		lookup[i] = 0;
 	}
 }
 
-void rfid_write_demo() {
-	print("start\n");
-	led_set_color(0xff0000);
-	belt_big_set_speed(0);
-	belt_small_set_speed(0);
-	servo_angle_set(0);
-	subscribe_to_event(EVENT_RFID_DETECT);
-	enum EventType e;
-	while (1) {
-		e = next_event();
-		if (e == EVENT_RFID_DETECT) {
-			led_set_color(0x00ff00);
-			char data[4];
-			data[0] = 0x1;
-			RFID_write_data_block((int)data, 0);
-			data[0] = 0x1;
-			RFID_write_data_block((int)data, 1);
-			data[0] = 0x56;
-			RFID_write_data_block((int)data, 2);
-			data[0] = 0x0;
-			RFID_write_data_block((int)data, 3);
-			data[0] = 0x0;
-			RFID_write_data_block((int)data, 6);
-			data[0] = 0x1;
-			RFID_write_data_block((int)data, 7);
-			data[0] = 0x34;
-			RFID_write_data_block((int)data, 8);
-			subscribe_to_event(EVENT_RFID_DETECT);
-			print("end\n");
+bool add_task(int move) {
+	if (next_free == current) {
+		// Epic fail
+		return false;
+	}
+	Task task;
+	task.move = move;
+	task.request = current_request;
+
+	tasks[next_free] = task;
+	next_free = (next_free + 1) % 7;
+	return true;
+}
+
+bool do_task() {
+	switch (tasks[current].move) {
+		case IDLE:
+			break;
+		case MOVE_RFID_TO_LEFT:
+			// PUT IN ID 
+			move_within_module(0, RFID, LASER_LEFT);
+			state.at_left = true;
+			state.at_RFID = false;
+			break;
+		case MOVE_LEFT_TO_RFID:
+			// PUT IN ID 
+			move_within_module(0, LASER_LEFT, RFID);
+			state.at_left = false;
+			state.at_RFID = true;
+			break;
+		case MOVE_RFID_TO_RIGHT:
+			// PUT IN ID 
+			move_within_module(0, RFID, LASER_RIGHT);
+			state.at_right = true;
+			state.at_RFID = false;
+			break;
+		case MOVE_RIGHT_TO_RFID:
+			// PUT IN ID 
+			move_within_module(0, LASER_RIGHT, RFID);
+			state.at_right = false;
+			state.at_RFID = true;
+			break;
+		case MOVE_LEFT_TO_RIGHT:
+			// PUT IN ID 
+			move_within_module(0, LASER_LEFT, LASER_RIGHT);
+			state.at_left = false;
+			state.at_right = true;
+			break;
+		case MOVE_RIGHT_TO_LEFT:
+			// PUT IN ID 
+			move_within_module(0, LASER_RIGHT, LASER_LEFT);
+			state.at_right = false;
+			state.at_left = true;
+			break;
+		case LEAVE_AT_LEFT:
+			// PUT IN ID 
+			request_leave(0, next.left, tasks[current].request);
+			state.at_left = false;
+			break;
+		case LEAVE_AT_RIGHT:
+			// PUT IN ID 
+			request_leave(0, next.right, tasks[current].request);
+			state.at_right = false;
+			break;
+		case LEAVE_AT_RFID:
+			// PUT IN ID 
+			request_leave(0, next.left, tasks[current].request);
+			state.at_RFID = false;
+			break;
+	}
+	tasks[current].move = IDLE;
+	current = (current + 1) % 7;
+	return true;
+}
+
+void handle_request() {
+	// Reroute tub if it's plane has arrived
+	
+	if(!current_request.plane_arrived && planes[current_request.plane_ID] != 0) {
+		current_request.destination = planes[current_request.plane_ID];
+		current_request.plane_arrived = true;
+	}
+
+	int which_module = lookup[current_request.destination];
+
+	if (current_request.sender_ID == next.left) {
+		add_task(WAIT_AT_LEFT);
+		if(which_module == next.rfid) {
+			add_task(MOVE_LEFT_TO_RFID);
+			add_task(LEAVE_AT_RFID);
+		} else {
+			add_task(MOVE_LEFT_TO_RIGHT);
+			add_task(LEAVE_AT_RIGHT);
 		}
-		sleep(100);
+	} else if (current_request.sender_ID == next.right) {
+		add_task(WAIT_AT_RIGHT);
+		if(which_module == next.rfid) {
+			add_task(MOVE_RIGHT_TO_RFID);
+			add_task(LEAVE_AT_RFID);
+		} else {
+			add_task(MOVE_RIGHT_TO_LEFT);
+			add_task(LEAVE_AT_LEFT);
+		}
+	} else if (current_request.sender_ID == next.rfid) {
+		add_task(WAIT_AT_RFID);
+		if(which_module == next.left) {
+			add_task(MOVE_RFID_TO_LEFT);
+			add_task(LEAVE_AT_LEFT);
+		} else {
+			add_task(MOVE_RFID_TO_RIGHT);
+			add_task(LEAVE_AT_RIGHT);
+		}
 	}
 }
 
-void rfid_read_demo() {
-	print("start\n");
-	led_set_color(0xff0000);
-	belt_big_set_speed(0);
-	belt_small_set_speed(0);
-	servo_angle_set(0);
-	subscribe_to_event(EVENT_RFID_DETECT);
+void get_request() {
 	enum EventType e;
-	while (1) {
-		e = next_event();
-		if (e == EVENT_RFID_DETECT) {
-			led_set_color(0x00ff00);
-			int rfid = get_security_flag();
-			printf("Security: %02X\n", rfid);
-			rfid = get_plane_dropoff_flag();
-			printf("Plane/Dropoff: %02X\n", rfid);
-			rfid = get_plane_id();
-			printf("Plane: %02X\n", rfid);
-			rfid = get_payload();
-			printf("Payload: %02X\n", rfid);
-			rfid = has_security_been_passed();
-			printf("Passed Security: %02X\n", rfid);
-			rfid = has_plane_arrived();
-			printf("Plane Arrived: %02X\n", rfid);
-			rfid = get_destination();
-			printf("Destination: %02X\n", rfid);
-			subscribe_to_event(EVENT_RFID_DETECT);
-			print("end\n");
+	while ((e = next_event())) {
+		char* msg;
+		if(e == EVENT_MESSAGE_RECEIVED) {
+			handle_message(&msg, 0);
+		} else {
+			// Handle other events ?
 		}
-		sleep(100);
 	}
+}
+	
+
+void loop() {
+	if(tasks[current].move == IDLE) {
+		get_request();
+		handle_request();
+	}
+	do_task();
+
+	sleep(50);
 }
 
 export int main(void) {
-	demo();
+	init(3, 3, 0);
+	for (int i = 0; i < 100000000; i++)
+	{
+		loop();
+	}
 }
